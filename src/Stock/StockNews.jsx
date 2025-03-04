9e8d0aeed39b0e5ab9739c5f12dde0c1a3d7ee26
Jeong Jaeyoon <jyoon3741@naver.com>
2025-03-04 13:55:15 +0900

    feat: Update StockNews.jsx 파일생성

--- src/Stock/StockNews.jsx
+++ src/Stock/StockNews.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./StockBegin.css";
 
 function StockNews(props) {
     return (
         <div>
-            
+                        
         </div>
     );
 }
