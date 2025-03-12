14778effe33eea339a977822968081daee6e3406
dmstn0526 <dmstn_0526@naver.com>
2025-03-12 18:12:25 +0900

    [fix]: header, app css 수정 (#8)

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -7,8 +7,12 @@ const Header = ({ title }) => {
   const navigate = useNavigate(); //페이지 이동을 위한 훅
   return (
     <div className="header">
-      <IoIosArrowBack className="back-icon" onClick={() => navigate(-1)} />
-      <h1 className="header-title">{title}</h1>
+      <span className="back-icon-container">
+        <IoIosArrowBack className="back-icon" onClick={() => navigate(-1)} />
+      </span>
+      <span className="header-title-container">
+        <h1 className="header-title">{title}</h1>
+      </span>
     </div>
   );
 };
