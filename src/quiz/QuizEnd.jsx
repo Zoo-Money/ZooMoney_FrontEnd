dc1429dda008792ab4ac0c8278f3cf636f9ea535
aneunoun <117334297+aneunoun@users.noreply.github.com>
2025-03-12 21:37:08 +0900

    quiz 번호 매기기 오늘의 quiz 포인트(#3)

--- src/quiz/QuizEnd.jsx
+++ src/quiz/QuizEnd.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./quizEnd.css";
 import Footer from "../common/Footer";
 import giraffe1 from "../images/quiz/giraffe_end.png";
 import Header from "../common/Header";
+import axios from "axios";
 
 const QuizEnd = () => {
+  const [correctAnswerCount, setCorrectAnswerCount] = useState(0); // 맞춘 퀴즈 데이터 개수를 저장할 상태
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:7777/zoomoney/quiz/total")
+      .then((response) => {
+        console.log("✅ 백엔드 응답:", response.data); // 백엔드 응답 확인
+        setCorrectAnswerCount(response.data.correctAnswerCount); // 상태 업데이트
+      })
+      .catch((error) =>
+        console.error("맞춘 퀴즈의 개수를 알 수 없습니다.", error)
+      );
+  }, []);
+
   return (
     <div className="mock-container">
       {/* 헤더 */}
@@ -17,7 +32,8 @@ const QuizEnd = () => {
         <img src={giraffe1} alt="퀴즈완료 캐릭터" className="quizend-image" />
         <p className="quizend-description">
           오늘의 퀴즈를 모두 풀었어요
-          <br />총 <strong>300 포인트</strong>를 획득했어요!
+          <br />총 <strong>{String(correctAnswerCount) * 100} 포인트</strong>를
+          획득했어요!
         </p>
         <p className="quizend-description">내일 다시 만나요!</p>
         <button className="quizend-button">퀴즈 풀이 완료</button>
--- src/quiz/QuizSuccess.jsx
+++ src/quiz/QuizSuccess.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./quizSuccess.css";
 import Footer from "../common/Footer";
 import giraffeQuiz from "../images/quiz/giraffe_quiz.png";
 import Header from "../common/Header";
 import O from "../images/quiz/O.png";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const QuizSuccess = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const quiz = location.state?.quiz; // ✅ 전달된 퀴즈 데이터 가져오기
 
+  // const nextQuiz = () => {
+  //   axios
+  //     .get("http://localhost:7777/zoomoney/quiz/count")
+  //     .then((response) => {
+  //       console.log("✅ 백엔드 응답:", response.data); // 백엔드 응답 확인
+
+  //       const quizCount = response.data.quizCount; // 백엔드에서 받은 data수
+  //       if (quizCount >= 5) {
+  //         // 누적 퀴즈 data수가 5개 이상이면
+  //         navigate("/quiz/end"); // 종료 페이지로 이동
+  //       } else {
+  //         // 5개 미만이면
+  //         navigate("/quiz/quiz"); // 퀴즈 출제 페이지로 이동
+  //       }
+  //     })
+  //     .catch((error) => console.error("퀴즈의 개수를 알 수 없습니다.", error));
+  // };
+
+  const [quizCount, setQuizCount] = useState(0); // 퀴즈 데이터 개수를 저장할 상태
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:7777/zoomoney/quiz/count")
+      .then((response) => {
+        console.log("✅ 백엔드 응답:", response.data); // 백엔드 응답 확인
+        setQuizCount(response.data.quizCount); // 상태 업데이트
+      })
+      .catch((error) => console.error("퀴즈의 개수를 알 수 없습니다.", error));
+  }, []);
+
   const nextQuiz = () => {
-    navigate("/");
+    if (quizCount >= 5) {
+      // 누적 퀴즈 data수가 5개 이상이면
+      navigate("/quiz/end"); // 종료 페이지로 이동
+    } else {
+      // 5개 미만이면
+      navigate("/quiz/quiz"); // 퀴즈 출제 페이지로 이동
+    }
   };
 
   return (
@@ -26,7 +63,9 @@ const QuizSuccess = () => {
       {/* 메인 콘텐츠 */}
       <div className="quizsuccess-content">
         <div className="quizsuccess-num-image">
-          <p className="quizsuccess-number">QUIZ 01</p>
+          <p className="quizsuccess-number">
+            QUIZ {String(quizCount).padStart(2, "0")}
+          </p>
           <img
             src={giraffeQuiz}
             alt="퀴즈성공 캐릭터"
