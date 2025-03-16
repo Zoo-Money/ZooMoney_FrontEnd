14e7ef2e270823ac5f43cd45ec40877130a69a6f
Jeong Jaeyoon <jyoon3741@naver.com>
2025-03-16 22:20:02 +0900

    [feat]:stockHistory 관련파일생성(#31)

--- src/stock/StockHistory.jsx
+++ src/stock/StockHistory.jsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../common/Header';
 import { IoIosArrowForward } from "react-icons/io";
 import "./stockHistory.css";
+import axios from 'axios';
+
 function StockHistory(props) {
+    const [ranking, setRanking] = useState([]);
     const navi = useNavigate();
-    const goHistoryDetail = ()=>{
-        navi("/stock/stockHistoryDetail");
+    const goHistoryDetail = (item)=>{
+        navi("/stock/stockHistoryDetail", {state: {item}});
+    };
+
+    useEffect(()=>{
+        axios({
+          url: "http://localhost:7777/zoomoney/stock/result/list",
+          method: "get",
+        })
+          .then((responseData)=>{
+            setRanking(responseData.data);
+          })
+          .catch((err)=>{
+            console.log(err);
+          });
+    },[]);
+
+    const afterOneWeek = (date) => {
+        const d = new Date(date);
+        d.setDate(d.getDate()+6);
+        return d.toISOString().split('T')[0];
     };
     return (
-      <div className="mock-container">
-        <Header title="나의 랭킹 히스토리"></Header>
-        <button className="history-button">내 투자 내역 보러가기</button>
-        <div className="history-list-box">
-          <div className="history-list">
-            <span className='history-list-title'>시즌 1</span>
-            <span className='history-list-date'>2024.03.01. ~ 2024.03.31.</span>
-            <span className='history-list-rank'>28위</span>
-            <span>
-              <IoIosArrowForward className="forwardIcon" onClick={goHistoryDetail} />
-            </span>
-          </div>
+        <div className="mock-container">
+            <Header title="나의 랭킹 히스토리"></Header>
+            <button className="history-button">내 투자 내역 보러가기</button>
+            <div className="history-list-box">
+                {ranking && ranking.map((item, index)=>(
+                    <div className='history-list' key={index}>
+                        <span className='history-list-title'>시즌{index+1}</span>
+                        <span className='history-list-date'>{item.result_date} ~ {afterOneWeek(item.result_date)}</span>
+                        <span className='history-list-rank'>{item.result_rank}위</span>
+                        <IoIosArrowForward className="forwardIcon" onClick={() => goHistoryDetail(item)} />
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
     );
 }
 
--- src/stock/StockHistoryDetail.jsx
+++ src/stock/StockHistoryDetail.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import Header from '../common/Header';
 import rabbit05 from "../images/rabbit05.png";
+import { useLocation } from 'react-router-dom';
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, LineElement, PointElement, CategoryScale, LinearScale, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(LineElement, PointElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 function StockHistoryDetail(props) {
+    const location = useLocation();
+    const {item} = location.state || {};
+    
+    // const labels = data.map(item => item.result_date);
+    // const resultRate = data.map(item => item.result_rate);
+    // const chartData = {
+    //     labels: labels,
+    //     datasets: [
+    //         {
+    //             label: "Result Rate 변화",
+    //             data: resultRate,
+    //             borderColor: "blue",
+    //             borderWidth: 2,
+    //             pointBackgroundColor: ["red", "green"],
+    //             pointRadius: 8,
+    //             pointStyle: "circle",
+    //             fill: false
+    //         }
+    //     ]
+    // };
+    // const options = {
+    //     responsive: true,
+    //     scales: {
+    //         y: {
+    //             beginAtZero: false
+    //         }
+    //     }
+    // };
+
     return (
         <div className='mock-container'>
             <Header title="나의 투자 내역" />
@@ -11,12 +45,12 @@ function StockHistoryDetail(props) {
                     <img src={rabbit05} alt="rabbit05" />
                     <p>
                         지난 시즌 나의 등수는<br/>
-                        <span>87등</span> 이에요.
+                        <span>{item.result_rank}등</span> 이에요.
                     </p>
                 </div>
-                <div className="history-detail-chart">
-                    차트 들어갈 자리입니다.
-                </div>
+                {/* <div className="history-detail-chart">
+                    <Line data={chartData} options={options}></Line>
+                </div> */}
                 <div className="history-detail-list-box">
                     <div className="history-detail-list">
                         <span className='history-detail-list-title'>삼성전자</span>
