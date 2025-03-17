21099ff5ba9cd5d0a7c4d647486ecd536ec476c6
dmstn0526 <dmstn_0526@naver.com>
2025-03-17 13:12:49 +0900

    [feat]: 모의투자종목 상세 페이지 개발(#22)

--- src/stock/CompanyInfo.jsx
+++ src/stock/CompanyInfo.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Footer from "../common/Footer";
-import Header from "../common/Header";
 import { useParams } from "react-router-dom";
 import "./CompanyInfo.css";
-import { TbAB } from "react-icons/tb";
+
 const CompanyInfo = () => {
   // URL에서 종목 코드, 이름 가져오기
   const { stockId, stockName } = useParams();
@@ -28,23 +26,14 @@ const CompanyInfo = () => {
 
   return (
     <div>
-      <div className="header">
-        <Header title="회사 정보" />
-      </div>
-
-      {/* 회사 정보 섹션 */}
       <div className="company-info-container">
-        <h3>💡 {stockName}가 어떤 회사냐면요...</h3>
+        <h6>💡 {stockName}가 어떤 회사냐면요...</h6>
         <br />
         <ul className="company-info-list">
           {stockInfo.split("\n").map((line, index) => (
             <li key={index}>{line}</li>
           ))}
         </ul>
-      </div>
-
-      <div className="footer">
-        <Footer />
       </div>
     </div>
   );
