5ad61ead9a07bce5e4ad51e7a5ce9f304ab025f1
EJ0102 <dn2540@gmail.com>
2025-03-20 18:38:17 +0900

    [fix]: parentMain-notify 확인 (#129)

--- src/parent/ChildEventSend.jsx
+++ src/parent/ChildEventSend.jsx
@@ -118,7 +118,7 @@ const ChildEventSend = () => {
         { amount: Number(amount) }
       );
       toast.error("용돈 송금에 성공했습니다.");
-      navigate(`/parent/main?childNum=${storedChildNum}`); // childNum 전달
+      navigate(`/parent/main`); // childNum 전달
     } catch (error) {
       console.error("송금 실패:", error);
       toast.error("송금에 실패했습니다. 다시시도해주세요");
